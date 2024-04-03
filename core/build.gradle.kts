9e3824264690807e32ccb9c43a88258a1cd4e194
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-03 15:18:31 +0200

    🔧 Update Kotlin compilation configuration in `core` subproject

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins { kotlin("jvm") }
 
 kotlin.explicitApi()
@@ -7,6 +10,14 @@ dependencies {
     implementation(libs.kotools.types)
 
     testImplementation(libs.kotlin.test)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    javaPackagePrefix = "lamarque.loic.catest.core"
+    compilerOptions {
+        allWarningsAsErrors.set(true)
+        jvmTarget.set(JvmTarget.JVM_1_8)
+    }
 }
 
 tasks.test.configure(Test::useJUnitPlatform)
